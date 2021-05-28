d2ebb347632d3d1a150d4ef2d8501f24c62e0e22
BodLesyk <bodlesyk@gmail.com>
2021-05-28 11:48:21 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Aspose.Pdf.Facades;
+using System;
+using System.IO;
+
+namespace PDF_Merge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+
+            PdfFileEditor pdfEditor = new PdfFileEditor();
+            
+
+            //try
+            //{
+            //    pdfEditor.Concatenate("C://real.pdf", "C://Example.pdf", "C://Result.pdf");
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            FileStream outputStream = new FileStream("C://Merged.pdf", FileMode.Create);
+            FileStream[] streamArray = new FileStream[3];
+            streamArray[0] = new FileStream("C://real.pdf", FileMode.Open);
+            streamArray[1] = new FileStream("C://Example.pdf", FileMode.Open);
+            streamArray[2] = new FileStream("C://empty.pdf", FileMode.Open);
+
+            try
+            {
+                pdfEditor.Concatenate(streamArray, outputStream);
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
